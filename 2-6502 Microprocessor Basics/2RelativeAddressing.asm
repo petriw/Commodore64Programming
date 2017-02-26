cf90a8760a5ba635de167f8fd13d480bfac43ad6
petriw <petriw@gmail.com>
2017-02-25 16:34:06 -0800

    Adding tutorial 2.2 and 2.3

---
+++ 2-6502 Microprocessor Basics/2RelativeAddressing.asm
@@ -0,0 +1,9 @@
+	processor 6502
+	org $1000
+
+loop:
+	ldx #$20
+	lda #$03
+	sta $d000,X
+	sta $d001,X
+	jmp loop
---
+++ 2-6502 Microprocessor Basics/3IncDecRegisters.asm
@@ -0,0 +1,9 @@
+	processor   6502
+	org    $1000
+
+loop:
+	ldy #$03
+	sty $d020
+	iny
+	sty $d021
+	jmp loop
